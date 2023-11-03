f0dc3eafbe806ed7275684c95bf44bdd76ace5fc
jorgeguedess <jorge_guedes00@hotmail.com>
2023-11-02 22:35:58 -0300

    feat: add type button

---
+++ src/types/button.d.ts
@@ -0,0 +1,15 @@
+export interface IButtonProps {
+  classGrid?: string;
+  visibleText?: boolean;
+  hiddenTag?: boolean;
+  label?: boolean;
+  text?: string;
+  active?: boolean;
+  variant?: string;
+  disabledShadow?: boolean;
+  disabled?: boolean;
+  startIcon?: string;
+  endIcon?: string;
+  color?: string;
+  size?: string;
+}
